ff672dd63eec27a6b4143536307172d71883447e
Ronyda Essam <ronyda988@gmail.com>
2026-06-07 13:36:41 +0300

    add try/catch error handling

--- src/handlers/notes-handler.ts
+++ src/handlers/notes-handler.ts
@@ -3,50 +3,66 @@ import { prisma } from "../lib/prisma";
 import { createNoteSchema } from "../lib/schemas";
 
 const getNotes = async (c: Context) => {
-    const notes = await prisma.note.findMany();
+    try {
+        const notes = await prisma.note.findMany();
 
-    return c.json({ notes });
+        return c.json({ notes });
+    } catch (error) {
+        return c.json({ error: 'something went wrong' }, 500)
+    }
 }
 
 const getNoteById = async (c: Context) => {
-    const reqId = Number(c.req.param('id'));
-    const note = await prisma.note.findUnique({ where: { id: reqId } });
+    try {
+        const reqId = Number(c.req.param('id'));
+        const note = await prisma.note.findUnique({ where: { id: reqId } });
 
-    if (!note) {
-        return c.json({ error: 'note not found' })
+        if (!note) {
+            return c.json({ error: 'note not found' })
+        }
+
+        return c.json({ note });
+    } catch (error) {
+        return c.json({ error: 'something went wrong' }, 500)
     }
-
-    return c.json({ note });
 }
 
 const createNote = async (c: Context) => {
-    const body = await c.req.json();
-    const result = createNoteSchema.safeParse(body);
+    try {
+        const body = await c.req.json();
+        const result = createNoteSchema.safeParse(body);
 
-    if (!result.success) {
-        return c.json({ error: 'Invalid Input', message: 'make sure title and body are not empty' }, 400)
-    }
-
-    const newNote = await prisma.note.create({
-        data: {
-            title: result.data.title,
-            body: result.data.body
+        if (!result.success) {
+            return c.json({ error: 'Invalid Input', message: 'make sure title and body are not empty' }, 400)
         }
-    });
 
-    return c.json({ message: "success", params: newNote });
+        const newNote = await prisma.note.create({
+            data: {
+                title: result.data.title,
+                body: result.data.body
+            }
+        });
+
+        return c.json({ message: "success", params: newNote });
+    } catch (error) {
+        return c.json({ error: 'something went wrong' }, 500)
+    }
 }
 
 const deleteNote = async (c: Context) => {
-    const reqId = Number(c.req.param('id'));
-    const noteId = await prisma.note.findUnique({ where: { id: reqId } });
+    try {
+        const reqId = Number(c.req.param('id'));
+        const noteId = await prisma.note.findUnique({ where: { id: reqId } });
 
-    if (!noteId) {
-        return c.json({ error: 'note Id not found' })
+        if (!noteId) {
+            return c.json({ error: 'note Id not found' })
+        }
+
+        await prisma.note.delete({ where: { id: reqId } });
+        return c.json({ message: `note ${reqId} deleted!` });
+    } catch (error) {
+        return c.json({ error: 'something went wrong' }, 500)
     }
-
-    await prisma.note.delete({ where: { id: reqId } });
-    return c.json({ message: `note ${reqId} deleted!` });
 }
 
 export { getNotes, getNoteById, createNote, deleteNote };
