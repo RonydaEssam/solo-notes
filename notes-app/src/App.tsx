933424c50b1e3bcf8f72652a2ad055b2f4941b03
Ronyda Essam <ronyda988@gmail.com>
2026-06-10 12:16:51 +0300

    quick fix

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -3,13 +3,17 @@ import './App.css'
 import { Footer } from './Footer'
 import { Header } from './Header'
 import type Note from "../../src/lib/types";
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+const port = Number(process.env.PORT);
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
 
   useEffect(() => {
     const fetchNotes = async () => {
-      const response = await fetch('http://localhost:3000/notes');
+      const response = await fetch(`http://localhost:${port}/notes`);
       const data = await response.json();
 
       setNotes(data.notes);
